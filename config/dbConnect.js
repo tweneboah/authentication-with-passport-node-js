30d3b7ce6133ac8ed8792cbf09abf0437f5283e5
tweneboah <twentekghana@gmail.com>
2020-09-26 12:26:06 +0800

    Backend complete

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
   mongoose.connect(
-    'mongodb://127.0.0.1:27017/Passport-React-Tut',
+    process.env.MONGO_URL_LOCAL,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
